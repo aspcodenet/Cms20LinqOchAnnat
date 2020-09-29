ff362d0de98ba1b1aa1d22ae1ed659a606f2c54a
Stefan Holmberg <stefan.holmberg@systementor.se>
2020-09-29 13:01:17 +0200

    Add project files.

---
+++ Cms20LinqOchAnnat/Player.cs
@@ -0,0 +1,25 @@
+﻿namespace Cms20LinqOchAnnat
+{
+    public class Player
+    {
+        //private int age;
+        //private readonly int age;
+
+        //private static int MaxAge = 49;
+
+        public int Age { get; set; }
+
+        public string Name { get; set; }
+        public string Season { get; set; }
+        public string Team { get; set; }
+        public string Shoots { get; set; }
+        public string Position { get; set; }
+        public int GamesPlayed { get; set; }
+        public int Goal1s { get; set; }
+        public int Assists { get; set; }
+        public int Points { get; set; }
+        //public int PlusMinus { get; set; }
+        //public int PenaltyMinutes { get; set; }
+
+    }
+}
